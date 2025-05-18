6788e7187714d475ada359ec621e84d376ab22ef
Adrián <adrian.rodriguez.roqueiro@gmail.com>
2025-05-18 13:34:38 +0200

    Problema CON la animacion del enemigo arreglado AL FIN y problemas menores de diseño del level1

--- Assets/Scripts/controladorEnemigos.cs
+++ Assets/Scripts/controladorEnemigos.cs
@@ -5,22 +5,19 @@ public class MovimientoEnemigo : MonoBehaviour
 {
     // Velocidad de movimiento del enemigo. Editable desde el Inspector.
     [SerializeField] private float velocidad = 2f;
-    //Prefab del power-up
+    // Prefab del power-up
     [SerializeField] private GameObject prefabPowerUp;
     // Sonido al morir
     [SerializeField] private AudioClip muerteSound;
 
     // Indica si el enemigo se está moviendo hacia la derecha.
     private bool moviendoDerecha = true;
-    private Animation caminarEnemigo;
-    Animator anim = null;
 
     // Método llamado al iniciar el juego o al activarse el objeto.
     void Start()
     {
         // Asigna la etiqueta "Enemigo" a este GameObject
         gameObject.tag = "Enemigo";
-        anim = GetComponent<Animator>();
     }
 
     // Método llamado una vez por frame
@@ -29,7 +26,6 @@ public class MovimientoEnemigo : MonoBehaviour
         // Mueve al enemigo horizontalmente.
         // Si 'moviendoDerecha' es true, se mueve hacia la derecha, si no, hacia la izquierda.
         transform.Translate(Vector2.right * velocidad * Time.deltaTime * (moviendoDerecha ? 1 : -1));
-        caminarEnemigo.Play();
     }
 
     // Método que se ejecuta al colisionar con otro objeto 2D
@@ -72,6 +68,15 @@ public class MovimientoEnemigo : MonoBehaviour
         transform.localScale = escala;
     }
 }
+
+
+///-----------------------------------------------
+/// ANOTACION IMPORTANTE 
+/// SE CORRIGIO EL ERROR DEL ANIMATOR ASIGNANDOLE UNA ANOMACION POR DEFECTO QUE SIEMPRE USARA DEDE UNITY DE CAMINAR,
+///QUE SE EJECUTARA SIEMPRE ASI NO DARA ERROR EN LA ANIMACION.
+///-----------------------------------------------
+
+
 
 
 
