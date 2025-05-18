160b5e8e570de02220897606b4d854616871ab1e
Adrián <adrian.rodriguez.roqueiro@gmail.com>
2025-05-18 13:52:16 +0200

    El power up parpadea

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -5,6 +5,18 @@ public class PowerUps : MonoBehaviour
     public int vidasParaSumar = 1;
     [SerializeField] private AudioClip vidaSong;
 
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // Hace que el sprite parpadee
+        spriteRenderer.enabled = Mathf.FloorToInt(Time.time * 5) % 2 == 0;
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Jugador"))
